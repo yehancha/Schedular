21b12911adada112853d7bec9c95e0e54a1dba16
Yehan Pemarathne <yehancha@gmail.com>
2014-04-06 23:27:04 +0530

    Now we can read the input file

--- RoundRobin.c
+++ RoundRobin.c
@@ -4,5 +4,15 @@ int main() {
 	printf("RR simulation$ ");
 	char input[20];
 	scanf("%s", input);
-	printf("Input is %s\n", input);
+	printf("Input file is %s\n", input);
+	FILE* file = fopen(input, "r");
+
+	char line[20];
+	int count = 0;
+	while (fgets(line, sizeof(line), file) != NULL) {
+	 // printf("#%d: %s", count++, line);
+	  int arrival, burst;
+	  sscanf(line, "%d\t%d", &arrival, &burst);
+	  printf("%d %d", arrival, burst);
+	}
 }
