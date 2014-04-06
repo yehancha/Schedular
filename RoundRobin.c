0f9d4c9ed310454310834914f54bebd4a67caa39
Yehan Pemarathne <yehancha@gmail.com>
2014-04-07 00:30:08 +0530

    RoundRobin.c is now structered well. We have a structure to store Program data (arrival time, burst time) We have a new function to create a Program Linked List with all the data read from the input file

--- RoundRobin.c
+++ RoundRobin.c
@@ -1,18 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Program {
+  int arrival;
+  int burst;
+  struct Program * next;
+} Program;
+
+Program * newProgram(int arrival, int burst) {
+  Program * program = (Program *) malloc(sizeof(Program));
+  program->arrival = arrival;
+  program->burst = burst;
+  program->next = 0;
+  return program;
+}
+
+Program * createProgramList(FILE * inputFile) {
+  char line[20];
+	Program * home = 0, * current;
+	while (fgets(line, sizeof(line), inputFile) != NULL) {
+	  int arrival, burst;
+	  sscanf(line, "%d\t%d", &arrival, &burst);
+	  Program * temp = newProgram(arrival, burst);
+	  if (home != 0) {
+	    current->next = temp;
+	  } else {
+	    home = temp;
+	  }
+	  current = temp;
+	}
+	return home;
+}
 
 int main() {
 	printf("RR simulation$ ");
 	char input[20];
 	scanf("%s", input);
 	printf("Input file is %s\n", input);
-	FILE* file = fopen(input, "r");
+	FILE * file = fopen(input, "r");
 
-	char line[20];
-	int count = 0;
-	while (fgets(line, sizeof(line), file) != NULL) {
-	 // printf("#%d: %s", count++, line);
-	  int arrival, burst;
-	  sscanf(line, "%d\t%d", &arrival, &burst);
-	  printf("%d %d", arrival, burst);
+  Program * home;
+  if (file != NULL) {
+	  home = createProgramList(file);
+  } else {
+    exit(1);
+  }
+	
+	Program * current = home;
+	while (current != 0) {
+	  printf("%d %d\n", current->arrival, current->burst);
+	  current = current->next;
 	}
 }
