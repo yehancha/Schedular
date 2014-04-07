94ca7db6ae03f71f75687575fdf311e18f66cc6a
Yehan Pemarathne <yehancha@gmail.com>
2014-04-08 03:35:10 +0530

    RoundRobin fully implemented. Adjust input data so the Chart fits to a screen with more than 1000p width

--- RoundRobin.c
+++ RoundRobin.c
@@ -5,8 +5,13 @@
 typedef struct Program {
   int arrival;
   int burst;
+  int id;
   struct Program * next;
 } Program;
+
+int programsIn = 0; // number of programs came into tunning list. used when drawing the chart.
+int totalTime;
+
 
 FILE * getFile() {
 	printf("RR simulation$ ");
@@ -33,10 +38,12 @@ Program * newProgram(int arrival, int burst) {
 Program * createProgramList(FILE * inputFile) {
   char line[20];
 	Program * home = 0, * current;
+  int programCount = 0;
 	while (fgets(line, sizeof(line), inputFile) != NULL) {
 	  int arrival, burst;
 	  sscanf(line, "%d\t%d", &arrival, &burst);
 	  Program * temp = newProgram(arrival, burst);
+	  temp->id = programCount++;
 	  if (home != 0) {
 	    current->next = temp;
 	  } else {
@@ -47,27 +54,78 @@ Program * createProgramList(FILE * inputFile) {
 	return home;
 }
 
+int calTotalTime(Program * programList) {
+  totalTime = 0;
+  
+  if (programList != 0) {
+    totalTime = programList->arrival; // Time the first program comes in
+  }
+  
+  while (programList != 0) {
+    totalTime += programList->burst;
+    programList = programList->next;
+  }
+  
+  return totalTime;
+}
+
+void printChart(int chart[]) {
+  int i = 0;
+	while (i < programsIn) {
+	  printf("#%d:\t", i);
+	  int j = 0;
+	  while (j < totalTime) {
+	    if (chart[j] == i) {
+	      printf("*");
+	    } else {
+	      printf(" ");
+	    }
+	    j++;
+	  }
+	  printf("\n");
+	  i++;
+	}
+}
+
 int main() {
 	FILE * file = getFile();
   validateFile(file);
   
   Program * initialList = createProgramList(file);
+  calTotalTime(initialList);
+  
+  // Stores chart information.
+  // chart tells what program was working at each clockTick
+  // Ex: chart[13] = 4
+  // This tells that in 13th clockTick, #4 program was working. "4" is program id.
+  int chart[totalTime];
+  // Assign all clockTicks to -1, so they will not represent any program id when starts.
+  int i = 0;
+  while (i < totalTime) {
+    chart[i] = -1;
+    i++;
+  }
 	
-	Program * current = initialList;
-	while (current != 0) {
-	  printf("%d %d\n", current->arrival, current->burst);
-	  current = current->next;
-	}
-	
-	Program * runningList = 0;
+	Program * runningList = 0; // Will contain running programs at that particular time
 	Program * runningTail = 0; // Tail of runningList
+	
+	int timeQuantum = 3;
+	int timeSpent = 0; // time spent by the current program
 	
 	int clockTick = 0;
 	while (initialList != 0 || runningList != 0) { // If we have programs in initialList or runningList
+	  
+  	printf("\033[2J\033[1;1H"); // clears terminal
+  	printf("ClockTick: %d\n", clockTick);
 	
 	  // Simulating a program coming in
-	  while (initialList != 0) {
+	  int programsComing = 0; // 0=program didn't come in, 1=program came in
+	  while (initialList != 0) { // If initialList is not empty
       if (initialList->arrival == clockTick) { // Program is comming in
+      
+        printf("#%d ", initialList->id);
+        programsComing = 1;
+        
         // Seperate the program from initialList
         Program * programComingIn = initialList;
         initialList = initialList->next;
@@ -82,15 +140,61 @@ int main() {
           runningTail = runningList;
         }
         
-        printf("New program comes at %d\n", clockTick);
+        programsIn++;
       } else { // Program is not coming in this clockTick
         break;
       }
 	  }
 	  
+	  if (programsComing == 1) {
+	    printf("coming in.\n");
+	  } else {
+	    printf("\n"); // just adding an empty line to maintain the same number of lines above the chart
+	  }
+	  
+	  if (runningList != 0) { // If the runningList is not empty
+	    runningList->burst--; // program spent 1 clockTick
+      printf("#%d is working (%d/%d, %d remains)\n", runningList->id, timeSpent + 1, timeQuantum, runningList->burst);
+      timeSpent++;
+      chart[clockTick] = runningList->id; // This program worked in this clockTick
+      
+	    if (runningList->burst == 0) { // Program has finished work
+	      printf("#%d finished work\n", runningList->id);
+	      // Removing finished program from runningList
+	      if (runningTail == runningList) { // If tail == head
+	        runningTail = runningList->next;
+	      }
+	      runningList = runningList->next;
+	      // Resetting timeSpent for the next program
+	      timeSpent = 0;
+	    } else if (timeSpent == timeQuantum) { // Time to change the program
+	      // Removing head of runningList
+	      if (runningList->next != 0) { // only if there's more than one program
+  	      Program * toTail = runningList;
+  	      runningList = runningList->next;
+  	      toTail->next = 0;
+  	      
+  	      // Adding it to the tail
+  	      runningTail->next = toTail;
+  	      runningTail = toTail;
+  	      printf("Switched from #%d to #%d\n", toTail->id, runningList->id);
+	      } else {
+	        printf("\n"); // just adding an empty line to maintain the same number of lines above the chart
+	      }
+	      
+	      // Resetting timeSpent for the next program
+	      timeSpent = 0;
+	    } else {
+	      printf("\n"); // just adding an empty line to maintain the same number of lines above the chart
+	    }
+	  }
+	  
 	  // increment clockTick
 	  clockTick++;
-	  // pausing 1sec. per clockTick
+	  
+	  printChart(chart);
+  	
+	  // pausing Xsec. per clockTick
 	  // (just to make the simulation attractive)
 	  // NOTE: In Linux, sleep takes the time in seconds while in Windows, it takes in milliseconds
 	  sleep(1);
